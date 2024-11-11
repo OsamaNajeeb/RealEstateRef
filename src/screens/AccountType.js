b03ca2d80d94236b18db1d3128121f44394173ca
OsamaNajeeb <20F20753@mec.edu.om>
2024-11-11 11:54:18 +0400

    Added custom font

--- src/screens/AccountType.js
+++ src/screens/AccountType.js
@@ -1,10 +1,14 @@
-import {View, Text} from 'react-native';
+import {View, Text, StatusBar} from 'react-native';
 import React from 'react';
 
 export default function AccountType() {
   return (
-    <View>
-      <Text>AccountType</Text>
+    <View style={{padding: 10}}>
+      <View style={{marginTop: 35}}>
+        <Text style={{color: 'black', fontFamily: 'Poppins-Bold'}}>
+          Job Finder
+        </Text>
+      </View>
     </View>
   );
 }
