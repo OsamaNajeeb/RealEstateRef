1d1168999c5b02e7dd45d029b41ddfa39f60e804
OsamaNajeeb <20F20753@mec.edu.om>
2024-11-11 11:23:34 +0400

    Updated Splash Screen

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,22 +1,33 @@
-import {View, Text, Image} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
+import {View, Image} from 'react-native';
+import CustomHomeIcon from '../assets/svg/customhome';
 
 export default function SplashScreen({navigation}) {
-  setTimeout(() => {
-    navigation.replace('acctype');
-  }, 2000);
+  // Redirect to 'acctype' screen after 2 seconds
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.replace('acctype');
+    }, 2000);
+    return () => clearTimeout(timer); // Cleanup the timer on component unmount
+  }, [navigation]);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#242424',
-        justifyContent: 'center', // Vertically center
-        alignItems: 'center', // Horizontally center
-      }}>
-      <Image
-        source={require('../assets/png/splashlogo.png')} // Use require for image paths
-        style={{width: 200, height: 200, resizeMode: 'contain'}}
-      />
+    <View style={{flex: 1, backgroundColor: '#242424'}}>
+      {/* Centered Image */}
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Image
+          source={require('../assets/png/splashlogo.png')}
+          style={{width: 200, height: 200, resizeMode: 'contain'}}
+        />
+      </View>
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          right: 0,
+        }}>
+        <CustomHomeIcon />
+      </View>
     </View>
   );
 }
