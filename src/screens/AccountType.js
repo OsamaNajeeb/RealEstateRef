75af0eb9b2e80bcfd43dc215cc335abb9fb5a735
OsamaNajeeb <20F20753@mec.edu.om>
2024-11-11 15:36:17 +0400

    Account Type Almost complete

--- src/screens/AccountType.js
+++ src/screens/AccountType.js
@@ -1,9 +1,13 @@
-import {View, Text, StatusBar} from 'react-native';
-import React from 'react';
+import {View, Text, StatusBar, Linking} from 'react-native';
+import React, {useState} from 'react';
+import AcctypeRectBtn from '../assets/components/AcctypeRectBtn';
+import NextRectangleIcon from '../assets/svg/customrecicon';
 
 export default function AccountType() {
+  const [activeButton, setActiveButton] = useState(null);
+
   return (
-    <View style={{padding: 15}}>
+    <View style={{padding: 15, flex: 1}}>
       <View style={{marginTop: 35}}>
         <Text style={{color: 'black', fontFamily: 'Poppins-Bold'}}>
           Job Finder
@@ -19,15 +23,50 @@ export default function AccountType() {
           Please select your profession down below:
         </Text>
       </View>
-      <View style={{marginHorizontal: 10, flex: 0.5, flexDirection: 'row'}}>
+      <View style={{flex: 1}}>
         <View
           style={{
-            backgroundColor: '#242424',
-            width: 150,
-            height: 150,
-            borderRadius: 20,
+            marginHorizontal: 10,
+            flexDirection: 'row',
+            marginTop: 20,
+            justifyContent: 'space-between',
           }}>
-          <View></View>
+          <AcctypeRectBtn
+            iconType={1}
+            label="Real Estate"
+            isClicked={activeButton === 'Real Estate'}
+            onPress={() => setActiveButton('Real Estate')}
+          />
+          <AcctypeRectBtn
+            iconType={0}
+            label="Plumber"
+            isClicked={activeButton === 'Plumber'}
+            onPress={() => setActiveButton('Plumber')}
+          />
+        </View>
+        <View
+          style={{
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            marginTop: 30,
+            position: 'relative',
+          }}>
+          <NextRectangleIcon />
+        </View>
+        <View style={{marginTop: 20, padding: 10}}>
+          <Text>
+            If your profession is not listed above please{' '}
+            <Text
+              style={{color: 'blue'}}
+              onPress={() =>
+                Linking.openURL(
+                  'https://www.youtube.com/watch?v=4wO6cYMYblo&list=PLeIJUF3PiXDfBaEA10imClVSzH-WzXFXt&index=3',
+                )
+              }>
+              contact
+            </Text>{' '}
+            us to include the requested profession.
+          </Text>
         </View>
       </View>
     </View>
